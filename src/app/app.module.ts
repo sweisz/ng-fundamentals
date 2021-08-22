510f5c0011555510b10359230739b0b60c5354b0
sweisz <dev@steffenweisz.de>
2021-08-22 23:09:47 +0200

    Hiding and Showing Content with ngSwitch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { NavBarComponent } from './nav/navbar.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {EventsAppComponent} from './events-app.component';
+import {EventsListComponent} from './events/events-list.component';
+import {EventThumbnailComponent} from './events/event-thumbnail.component';
+import {NavBarComponent} from './nav/navbar.component';
 
 @NgModule({
   imports: [
@@ -19,4 +18,5 @@ import { NavBarComponent } from './nav/navbar.component';
   providers: [],
   bootstrap: [EventsAppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,25 +1,42 @@
-import { Component, Input } from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 
 @Component({
-    selector: 'event-thumbnail',
-    template: `
-        <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}} </div>
-        <div>Time: {{event.time}} </div>
-        <div>Price: \${{event.price}} </div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            
-            <span class='pad-left'>{{event.location.city}}, {{event.location.country}}</span>
-        </div>
+  selector: 'event-thumbnail',
+  template: `
+    <div class="well hoverwell thumbnail">
+      <h2>{{event?.name}}</h2>
+      <div>Date: {{event?.date}} </div>
+      <div [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+      <div>Price: \${{event?.price}} </div>
+      <div *ngIf="event?.location">
+        <span>Location: {{event?.location?.address}}</span>
+        <span class='pad-left'>{{event?.location?.city}}, {{event?.location?.country}}</span>
+      </div>
+      <div *ngIf="event?.onlineUrl">
+        Online Url: {{event?.onlineUrl}}
+      </div>
     </div>
-    `,
-    styles:[`
-        .pad-left{margin-left:20px;}
-        .well div {color:#bbb}
-       
-    `]
+  `,
+  styles: [`
+    .pad-left {
+      margin-left: 20px;
+    }
+
+    .well div {
+      color: #bbb
+    }
+
+    .thumbnail {
+      min-height: 210px;
+    }
+
+
+  `]
 })
 export class EventThumbnailComponent {
   @Input() event: any
