594517e7dc8b1539463df4335bc64c987283fe71
sweisz <dev@steffenweisz.de>
2021-09-15 18:48:44 +0200

    Pre-Loading Data for Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {appRoutes} from "./routes";
 import {CreateEventComponent} from "./events/create-event.component";
 import {Error404Component} from "./errors/404.component";
 import {EventRouteActivator} from "./events/event-details/event-route-activator.service";
+import {EventListResolver} from "./events/events-list-resolver.service";
 
 @NgModule({
   imports: [
@@ -31,6 +32,7 @@ import {EventRouteActivator} from "./events/event-details/event-route-activator.
     EventService,
     ToastrService,
     EventRouteActivator,
+    EventListResolver,
     {
       provide: 'canDeactivateCreateEvent',
       useValue: checkDirtyState
