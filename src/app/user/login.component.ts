fb4036c68ba65a900c09b15eb9ddbd7c4475a7bd
sweisz <dev@steffenweisz.de>
2021-09-16 17:45:24 +0200

    Validating Template-based Forms2

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,10 +3,19 @@ import {AuthService} from "./auth.service";
 import {Router} from "@angular/router";
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: [`
+    em {
+      float: right;
+      color: #e95c65;
+      padding-left: .75rem;
+    }
+  `]
 })
 export class LoginComponent {
-
+  username: any
+  password: any
+  mouseoverLogin: any
 
   constructor(private authService: AuthService, private router: Router) {
   }
